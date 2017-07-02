03d3b649f8c9993d6b01c886f5c5629d22aeb6d1
Udomomo <batzuma@yahoo.co.jp>
2017-07-02 13:35:01 +0000

    make it work in both python2/3 | modified decode test

--- tests/test_vbcode.py
+++ tests/test_vbcode.py
@@ -14,23 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #---------------------------------------------------------------------------
+from __future__ import print_function
 import vbcode
 from struct import unpack
-
+import sys, random
 def test_vbc():
     # format() require python 2.6 or higher.
     def test_vb_encode(numbers, ok):
         bytestream = vbcode.encode(numbers)
         assert ''.join([format(b, '08b') for b in unpack('%dB' % len(bytestream), bytestream)]) == ok
-        print "test ok. %s -> %s" % (numbers, ok)
+        print("test ok. %s -> %s" % (numbers, ok))
     
-    test_vb_encode([1],   '10000001')
-    test_vb_encode([5],   '10000101')
-    test_vb_encode([127], '11111111')
-    test_vb_encode([128], '00000001' + '10000000')
-    test_vb_encode([129], '00000001' + '10000001')
-    
-    import sys, random
-    for i in xrange(1000):
-        n = random.randint(0, sys.maxint)
+    test_vb_encode([1],    '10000001')
+    test_vb_encode([5],    '10000101')
+    test_vb_encode([127],  '11111111')
+    test_vb_encode([128],  '00000001' + '10000000')
+    test_vb_encode([129],  '00000001' + '10000001')
+    test_vb_encode([1, 5], '10000001' + '10000101')
+
+    def test_vb_decode():    
+        n = random.randint(0, sys.maxsize)
         assert vbcode.decode(vbcode.encode([n]))[0] == n
+        print("test ok. %s -> %s" % (n, n))
+
+    test_vb_decode()
+
+test_vbc()
