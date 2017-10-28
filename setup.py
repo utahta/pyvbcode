a1b3aac0d9e35b1d85586ea3587653ed8292201b
utahta <labs.ninxit@gmail.com>
2017-10-28 23:07:46 +0900

    Update version
    Add MIT LICENSE

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
 # vim:fileencoding=utf8
-#---------------------------------------------------------------------------
-# Copyright 2011 utahta
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#---------------------------------------------------------------------------
 from distutils.core import setup
 import os
 README = os.path.join(os.path.dirname(__file__),'PKG-INFO')
 long_description = open(README).read() + "\n"
 
 setup(name="vbcode",
-      version='0.1',
+      version='0.2.0',
       py_modules=['vbcode'],
       description="Variable byte codes",
       author="utahta",
@@ -34,5 +19,5 @@ setup(name="vbcode",
                    "Natural Language :: Japanese"
                    ],
       url="https://github.com/utahta/pyvbcode",
-      license="http://www.apache.org/licenses/LICENSE-2.0"
+      license="MIT"
       )
